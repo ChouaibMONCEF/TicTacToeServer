60b1c7ba28c1c94013cbaf9bf7c129fc45f66d56
choaib moncef <choaibmoncef1@gmail.com>
2022-03-17 09:16:10 +0100

    Server Setup

---
+++ src/server.ts
@@ -0,0 +1,57 @@
+import "reflect-metadata";
+import app from "./app";
+var debug = require("debug")("socketio-server:server");
+import * as http from "http";
+import socketServer from "./socket";
+
+var port = normalizePort(process.env.PORT || "5000");
+app.set("port", port);
+
+var server = http.createServer(app);
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+const io = socketServer(server);
+
+function normalizePort(val: any) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " Requires elevated privileges");
+      process.exit(1);
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var address: any = server.address();
+  var bind =
+    typeof address === "string" ? "Pipe " + address : "Port " + address.port;
+  debug("Listening on " + bind);
+  console.log("Server Runing on Port: ", port);
+}
---
+++ src/socket.ts
@@ -0,0 +1,19 @@
+import {useSocketServer} from "socket-controllers"
+import {Server} from "socket.io"
+
+export default (httpServer: any) => {
+    const io = new Server(httpServer, {
+        cors: {
+            origin: "*"
+        },
+    })
+
+    // io.on("connection", (socket)=>{
+
+    // })
+
+    useSocketServer(io, {controllers: [__dirname + "api/controllers/*.ts"] })
+
+
+    return io;
+}
